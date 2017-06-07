4a88b0c6dbdfb7372eab67da32c4dbc03682adc1
wanggang <wanggang@udcredit.com>
2017-06-07 14:51:15 +0800

    update-code structrue

--- src/test/java/com/udcredit/demo/OrderQueryAPITest.java
+++ src/test/java/com/udcredit/demo/OrderQueryAPITest.java
@@ -1,6 +1,9 @@
 package com.udcredit.demo;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.udcredit.demo.util.MD5Utils;
+import com.udcredit.demo.util.TestCaseUtil;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,14 +13,13 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
-import com.udcredit.demo.util.MD5Utils;
-import com.udcredit.demo.util.TestCaseUtil;
 /**
- * Created by windows10 on 2017/5/17.
+ * 订单查询接口测试用例
+ *
  * @author wanglr
  * @date 2017-5-17
  */
-public class OrderQuery {
+public class OrderQueryAPITest {
     //TODO 线上地址见对接文档中的服务描述，商户公钥，商户私钥，商户开户时会下发到商户邮件
     //商户公钥
     final static String pub_key = "4ad2c7c4-f9fa-456b-92cd-056d5e5bcd59";
@@ -45,19 +47,19 @@ public class OrderQuery {
         OrderQueryTest("O_20170517130503");
     }
 
-        JSONObject OrderQueryTest(String partner_order_id)throws Exception{
+    JSONObject OrderQueryTest(String partner_order_id) throws Exception {
         JSONObject renJson = new JSONObject();
         String sign_time = TestCaseUtil.getStringDate(new Date());
-        String sign = getMD5Sign(pub_key, partner_order_id ,sign_time, security_key);
+        String sign = getMD5Sign(pub_key, partner_order_id, sign_time, security_key);
         System.out.println(sign);
-        renJson.put("partner_order_id",partner_order_id);
-        renJson.put("sign",sign);
-        renJson.put("sign_time",sign_time);
+        renJson.put("partner_order_id", partner_order_id);
+        renJson.put("sign", sign);
+        renJson.put("sign_time", sign_time);
 
-        System.out.println("查询接口参数：" + JSON.toJSONString(renJson,true));
+        System.out.println("查询接口参数：" + JSON.toJSONString(renJson, true));
 
         JSONObject order_query = TestCaseUtil.doHttpRequest(Order_Query, renJson);
-        System.out.println("查询接口查询结果：" + JSON.toJSONString(order_query,true));
+        System.out.println("查询接口查询结果：" + JSON.toJSONString(order_query, true));
         return order_query;
     }
 
